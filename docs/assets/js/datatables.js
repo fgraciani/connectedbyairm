13195fec4136b561fb3954fabec26cffea314740
fgraciani <fgraciani@gmail.com>
2020-10-07 11:00:55 +0200

    fixes datatables

--- docs/assets/js/datatables.js
+++ docs/assets/js/datatables.js
@@ -1,9 +1,6 @@
 	$(document).ready( function () {
 		$('#example').DataTable({
 		"ordering": true,
-		"info":     true,
-		"columnDefs": [
-		  { "width": "180px", "targets": 1 }
-		]
+		"info":     true
 		} );
 	} );
