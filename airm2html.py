655d3be93562a6ada4c7719b3af81fc633ca4ef7
fgraciani <fgraciani@gmail.com>
2020-10-08 07:57:17 +0000

    test6

--- airm2html.py
+++ airm2html.py
@@ -18,6 +18,7 @@ def create_html():
   import airm
   airm = airm.Airm()
   airm_logical_classes = airm.logical_classes.to_dict('records')
+  airm_logical_properties = airm.logical_properties.to_dict('records')
 
   #Create index page
   #creates soup for index using concept-list-template.html
@@ -55,6 +56,34 @@ def create_html():
     
     soup.find('tbody').insert(1,tr)
 
+  for record in airm_logical_properties:
+    tr = soup.new_tag("tr")
+    
+    td = soup.new_tag("td")
+    td.string = record["parent"]
+    tr.insert(1,td)
+
+    td_ic_name = soup.new_tag("td")
+    url = "LM/"+record["parent"]+".html#"+record["name"]
+    text = record["name"]
+    print(text)
+    new_link = soup.new_tag("a")
+    new_link['href'] = url
+    new_link['target'] = "_blank"
+    new_link.string = text
+    td_ic_name.insert(1,new_link)
+    tr.insert(2,td_ic_name)
+
+    if record["definition"] != "":
+      td_def = soup.new_tag("td")
+      td_def.string = str(record["definition"])
+      tr.insert(3,td_def)
+
+    td = soup.new_tag("td")
+    td.string = record["type"]
+    tr.insert(4,td)
+    
+    soup.find('tbody').insert(1,tr)
   f= open("docs/advanced-viewer/1.0.0/logical-model.html","w+")
   f.write(soup.prettify())
   f.close() 
