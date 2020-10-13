383ba8b1d8399c7b9a7a0a0a76cf786586673507
fgraciani <fgraciani@gmail.com>
2020-10-13 14:06:21 +0000

    amxm test 1

--- amxm.py
+++ amxm.py
@@ -2,12 +2,15 @@ import pandas as pd
 
 class Amxm:
   amxm_mapping_dataframe = pd.read_excel (r'data/xlsx/AMXM_Semantic_Correspondence_Report.xlsx', sheet_name='semantic_corresponence') #it would be nice to align sheet names
+  amxm_mapping_enum_dataframe = pd.read_excel (r'data/xlsx/AMXM_Semantic_Correspondence_Report.xlsx', sheet_name='codes_enums')
   #classes = pd.Dataframe() #TO DO load in init
   #properties = pd.Dataframe() #TO DO load in init
      
   def __init__(self):
     self.amxm_mapping_dataframe["AIRM Concept Identifier"] = self.amxm_mapping_dataframe["AIRM Concept Identifier"].str.strip()
     self.amxm_mapping_dataframe.fillna("missing data", inplace = True)
+    self.amxm_mapping_enum_dataframe["AIRM Concept Identifier"] = self.amxm_mapping_enum_dataframe["AIRM Concept Identifier"].str.strip()
+    self.amxm_mapping_enum_dataframe.fillna("missing data", inplace = True)
     #self.classes.fillna("missing data", inplace = True)
     #self.properties.fillna("missing data", inplace = True)
 
@@ -30,6 +33,18 @@ class Amxm:
     df_results = amxm_df.dropna(how='all')  
     
     if df_results.empty:
+      amxm_df = self.amxm_mapping_enum_dataframe.copy()
+    
+      filter = amxm_df["AIRM Concept Identifier"]==airm_urn
+      amxm_df.sort_values("AIRM Concept Identifier", inplace = True)
+      amxm_df.where(filter, inplace = True) 
+      df_results = amxm_df.dropna(how='all')
+      if df_results.empty:
+        return None
+      else:
+        results_dict = df_results.to_dict('records')
+        return results_dict
+
       return None
     else:
       results_dict = df_results.to_dict('records')
---
+++ amxm2html_old.py
@@ -0,0 +1,90 @@
+def old_create_html():
+  import airm
+  airm = airm.Airm()
+  #configuration
+  json_mapping_file = "data/json/fixm-mapping.json" #assets/js/fixm-mapping-test.json OR assets/js/fixm-mapping.json
+  
+
+  # creates developers/fixm-4.2.0-to-airm-1.0.0.html using developers/template.html
+  html = open("data/html/templates/template.html").read()
+  soup = BeautifulSoup(html, "lxml")
+
+ 
+
+  f= open("data/html/export/developers/fixm-4.2.0-to-airm-1.0.0.html","w+")
+  f.write(soup.prettify())
+  f.close() 
+
+  # creates assets/js/mapping-table.js injecting the contents of assets/js/fixm-mapping.json as values and using assets/js/mapping-table-template.js
+  with open(json_mapping_file) as json_file:
+    with open('data/html/templates/mapping-table-template.js') as fin, open('data/html/export/assets/js/mapping-table.js', 'w') as fout:
+        for line in fin:
+            if '        var values = [] ;' in line:
+                line = '        var values = '+ json_file.read()  +' ;\n'
+            fout.write(line)
+
+
+
+  # loads assets/js/fixm-mapping.json as a dictionary
+  with open(json_mapping_file) as json_file:
+    json_contents = json_file.read()
+  mapping_list = json.loads(json_contents)
+
+  # creates one page per concept in the mapping
+  html = open("data/html/templates/concept-template.html").read()
+  for trace in mapping_list:
+    #print(trace['Concept'])
+      
+    components = str(trace['Id']).split(":")
+    container_concept = components[1]
+    #TO-DO replace shortnames in id with full namespaces
+    full_id = str(trace['Id'])
+
+    # creates developers/fixm-4.2.0-to-airm-1.0.0/CONCEPT NAME.html using developers/concept-template.html
+    soup = BeautifulSoup(html, "lxml") 
+    # container concept for breadcrumb
+    soup.find(text="CONTAINER CONCEPT HERE").replace_with(container_concept)
+    # name for heading
+    soup.find(text="CONTAINER@CONCEPT NAME HERE").replace_with(trace['Concept'])
+    # ID for sub heading
+    soup.find(text="FULL ID HERE").replace_with(full_id)  
+    # definition 
+    soup.find(text="DEFINITION HERE").replace_with(str(trace['Definition']))
+    # type 
+    soup.find(text="TYPE HERE").replace_with(str(trace['Type']))
+    # rationale 
+    soup.find(text="RATIONALE HERE").replace_with(str(trace['Rationale']))
+    # notes 
+    soup.find(text="NOTES HERE").replace_with(str(trace['Notes']))
+
+    # semantic correspondence name and url 
+    new_link = BeautifulSoup('<a href="'+trace['url']+'">'+trace['Correspondence']+'</a>', "lxml")
+    row = soup.find(text="AIRM CONCEPT HERE").parent
+    row.clear()
+    row.append(new_link.a)
+
+    # AIRM concept definition 
+    entry = airm.load_and_find_urn(trace['urn'])
+    soup.find(text="AIRM CONCEPT DEFINITION HERE").replace_with(entry["definition"])
+        
+    # semantic correspondence url 
+    new_link = BeautifulSoup('<a href="'+trace['addurl']+'">'+trace['Additional']+'</a>', "lxml")
+    addrow = soup.find(text="AIRM ADDITIONAL CONCEPT HERE").parent
+    addrow.clear()
+    addrow.append(new_link.a)
+
+    
+
+    #page_content = BeautifulSoup("<div>"+container_concept+"</div>")
+    #soup.find(text="CONTAINER CONCEPT HERE").replace_with(page_content)
+    
+    filename = trace['Concept']
+    filename = filename.replace("@",".")
+    f= open(mapping_pages_directory+"/"+ filename +".html","w+")
+    
+    f.write(soup.prettify())
+    f.close() 
+
+  #DONE
+  print(str(airm.not_found_counter)+" AIRM URNs were not found")
+  print("Done")
