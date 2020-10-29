94b037fd3e8050ef9253ed7f2f2ea07a0aa21906
fgraciani <fgraciani@gmail.com>
2020-10-29 13:23:28 +0100

    Update navbarfix.js

--- docs/assets/js/navbarfix.js
+++ docs/assets/js/navbarfix.js
@@ -1,3 +1,3 @@
-var shiftWindow = function() { scrollBy(0, -120) };
+var shiftWindow = function() { scrollBy(0, -140) };
 if (location.hash) shiftWindow();
 window.addEventListener("hashchange", shiftWindow);
